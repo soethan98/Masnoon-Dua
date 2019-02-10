82fb72d0c037d39e1cd73fd90bff6cbd129b3706
soethan98 <soethan762@gmail.com>
2019-02-10 20:37:45 +0630

    Profile Page Added

---
+++ lib/data/dua_data.dart
@@ -0,0 +1,12 @@
+
+class Dua {
+  int dua_id;
+  String dua_title;
+  String dua_arbic;
+  String dua_desc;
+  String image_path;
+  String sound_url;
+
+  Dua(this.dua_id, this.dua_title, this.dua_arbic, this.dua_desc,
+      [this.image_path, this.sound_url]);
+}
--- lib/ui/profile_page.dart
+++ lib/ui/profile_page.dart
@@ -1,9 +1,126 @@
 import 'package:flutter/material.dart';
-
+import 'dart:ui' as ui;
 
 class ProfilePage extends StatelessWidget {
+  Widget _buildAvatar() {
+    return Container(
+      width: 110.0,
+      height: 110.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+      ),
+      padding: EdgeInsets.all(3.0),
+      child: ClipRect(child: Image.asset('assets/images/logo.png')),
+    );
+  }
+
+  Widget _buildCard() {
+    return Card(
+      color: Colors.black87,
+      margin: EdgeInsets.only(left: 10.0, right: 10.0),
+      elevation: 8.0,
+      child: Container(
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.only(bottom: 8.0,left: 8.0),
+        child: Column(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 8.0,vertical: 10.0),
+                child: Text(
+                  'Contact Us',
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Image.asset('assets/images/internet.png'),
+                SizedBox(
+                  width: 20.0,
+                ),
+                Text(
+                  'http://m2cs.org/',
+                  maxLines: 2,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Image.asset('assets/images/facebook.png'),
+                SizedBox(
+                  width: 20.0,
+                ),
+                Flexible(
+                  child: Text('https://www.facebook.com/M2CS-179991619396541/',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Image.asset('assets/images/email.png'),
+                SizedBox(
+                  width: 20.0,
+                ),
+                Text('m2cs786@gmail.com',
+                    style: TextStyle(color: Colors.white)),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text(
+          'Developed By',
+          style: TextStyle(
+              fontSize: 30.0,
+              color: Colors.white,
+              letterSpacing: 0.1,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'serif-monospace'),
+        ),
+        _buildAvatar(),
+
+        _buildCard(),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Center(
+              child:
+                  Image.asset('assets/images/m2cslogo.png', fit: BoxFit.cover)),
+          BackdropFilter(
+            filter: ui.ImageFilter.blur(sigmaX: 0.5, sigmaY: 0.5),
+            child: Container(
+              color: Colors.black.withOpacity(0.8),
+            ),
+          ),
+          _buildContent()
+        ],
+      ),
+    );
   }
 }
